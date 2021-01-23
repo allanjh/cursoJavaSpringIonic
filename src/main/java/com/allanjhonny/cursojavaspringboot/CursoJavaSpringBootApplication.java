62c50ee2ba9f702f9ff71a0fc00a4c0f2b6b9dd0
Allan Jhonny <allanjh@gmail.com>
2021-01-23 18:00:59 -0300

    Criacao de Projeto

---
+++ src/main/java/com/allanjhonny/cursojavaspringboot/CursoJavaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.allanjhonny.cursojavaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoJavaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoJavaSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allanjhonny/cursojavaspringboot/CursoJavaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allanjhonny.cursojavaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoJavaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
