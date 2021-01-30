4afbaea2b8d6f3667edf91844f1360b909566353
Allan Jhonny <allanjh@gmail.com>
2021-01-30 17:35:55 -0300

    criacao entidade Cidade e Estado

--- src/main/java/com/allanjhonny/cursojavaspringboot/CursoJavaSpringBootApplication.java
+++ src/main/java/com/allanjhonny/cursojavaspringboot/CursoJavaSpringBootApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.allanjhonny.cursojavaspringboot.domain.Categoria;
+import com.allanjhonny.cursojavaspringboot.domain.Cidade;
+import com.allanjhonny.cursojavaspringboot.domain.Estado;
 import com.allanjhonny.cursojavaspringboot.domain.Produto;
 import com.allanjhonny.cursojavaspringboot.repositories.CategoriaRepository;
+import com.allanjhonny.cursojavaspringboot.repositories.CidadeRepository;
+import com.allanjhonny.cursojavaspringboot.repositories.EstadoRepository;
 import com.allanjhonny.cursojavaspringboot.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class CursoJavaSpringBootApplication implements CommandLineRunner{
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoJavaSpringBootApplication.class, args);
@@ -41,8 +49,20 @@ public class CursoJavaSpringBootApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est1.getCidades().addAll(Arrays.asList(c2,c3));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 	}
 
 }
